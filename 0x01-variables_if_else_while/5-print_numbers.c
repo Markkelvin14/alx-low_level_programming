a3d5252a926be98732e02ac66b54c6309f10afea
Markkelvin14 <mjkelvin38@gmail.com>
2023-02-10 19:48:43 +0000

    Variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 10)
+	{
+		printf("%d\n", i);
+		i++;
+	}
+	return (0);
+}
