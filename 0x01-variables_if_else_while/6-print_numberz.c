4cfd8b9c5535fbd258058b8d76938f7d9883164f
Markkelvin14 <mjkelvin38@gmail.com>
2023-02-10 20:29:50 +0000

    Variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Answer 0
+ */
+int main(void)
+{
+	int i;
+	i = 0;
+	for (i = '0'; i <= '9'; i++)
+	putchar(i);
+	putchar('\n');
+
+	return (0);
+}
