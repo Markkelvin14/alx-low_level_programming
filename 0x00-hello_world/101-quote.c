366a6e1e2463eb2ec445af75f7026a1147391bda
Markkelvin14 <mjkelvin38@gmail.com>
2023-02-10 09:41:55 +0000

    Hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main - Entry
+ * Return: Always 1
+ */
+int main(void)
+{
+	write(2, "and that place of art is useful\" - Dora Korpar, 2015-10-19\n",39);
+	return (1);
+}	
