4a0c0e42afd9cbb78b99494cf43df76a9f54c323
Markkelvin14 <mjkelvin38@gmail.com>
2023-02-10 17:19:28 +0000

    Variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -Entry
+ * Return: Answer
+ */
+int main(void)
+{	
+	char c;
+	c = 'a';
+	while(c <= 'z')
+	{
+		putchar (c);
+		c++;
+	}
+	return (0);
+}	
