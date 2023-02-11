bd061fef9872f25528675e5cebfcb6fafe1065a7
Markkelvin14 <mjkelvin38@gmail.com>
2023-02-11 11:56:18 +0000

    Functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - sdds two integers and returns the result
+ * @m: interger to be added
+ * @n: interger to be added
+ * Return: sum
+ */
+int add(int m, int n)
+{
+	int i;
+
+	i = m + n;
+	return (i);
+}
