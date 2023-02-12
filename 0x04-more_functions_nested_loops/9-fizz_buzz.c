1f6ec5c0e28452dd28b352ba68d5cd283e892b0c
Markkelvin14 <mjkelvin38@gmail.com>
2023-02-12 08:17:10 +0000

    More functions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,7 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Fizz Buzz ");
+			printf("Fizzbuzz ");
 		}
 		else if (i % 3 == 0)
 		{
@@ -29,5 +29,6 @@ int main(void)
 			printf("%d ", i);
 		}
 	}
+	printf("\n");
 	return (0);
 }
