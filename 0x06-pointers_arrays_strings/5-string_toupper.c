84f46a631335db1ec3e1afb7932fc4d8bdab04f9
Markkelvin14 <mjkelvin38@gmail.com>
2023-03-01 21:02:19 +0000

    Arrays

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,13 +8,12 @@
 
 char *string_toupper(char *n)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-	while (n[i] != '\0')
+	while (n[i])
 	{
 		if (n[i] >= 'a' && n[i] <= 'Z')
-			n[i] = n[i] - 32;
+			n[i] -= 32;
 		i++;
 	}
 	return (n);
