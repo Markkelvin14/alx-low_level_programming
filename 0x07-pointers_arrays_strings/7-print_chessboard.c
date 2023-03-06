c9bc51c00e1855235058b039a4927f7e5f8be84f
Markkelvin14 <mjkelvin38@gmail.com>
2023-03-06 18:55:46 +0000

    More pointers

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: is a char
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int m;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (m = 0; m < 8; m++)
+			_putchar (a[i][m]);
+		_putchar ('\n');
+	}
+}
