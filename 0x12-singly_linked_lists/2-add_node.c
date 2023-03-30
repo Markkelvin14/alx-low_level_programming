5dc05154243c2b96d0e3e08da5d35348029789a5
Markkelvin14 <mjkelvin38@gmail.com>
2023-03-30 17:46:09 +0000

    Linked

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,7 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *newnode;
 	unsigned int len = 0;
 
-	while (str[len] != '\0;)
+	while (str[len] != '\0')
 		len++;
 	newnode = malloc (sizeof(list_t));
 	if (newnode == NULL)
