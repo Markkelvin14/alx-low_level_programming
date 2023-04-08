7deac73736858198d5f3b0c488c6f2ab8b82e7f2
Markkelvin14 <mjkelvin38@gmail.com>
2023-04-08 09:59:08 +0000

    File io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,15 +18,18 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	ptrfile = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wrte = write(ptrfile, text_content, stn);
-	if (ptrfile == -1 || wrte == -1)
-		return (-1);
 	if (text_content != NULL)
 	{
 		while (text_content[stn] > 0)
 			stn++;
 	}
+
+	ptrfile = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wrte = write(ptrfile, text_content, stn);
+	if (ptrfile == -1 || wrte == -1)
+		return (-1);
+	if (text_content != NULL)
+
 	close(ptrfile);
 	return (1);
 }
