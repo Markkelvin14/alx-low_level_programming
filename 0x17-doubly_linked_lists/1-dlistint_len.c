8da139db21ed04a5f1a4b45890a3d3d780914516
Markkelvin14 <mjkelvin38@gmail.com>
2023-05-18 12:53:00 +0000

    Doubly linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of a double linked list
+ * h: head of the list
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int m = 0;
+
+	while (h == NULL)
+		return (0);
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h != NULL)
+	{
+		m++;
+		h = h->next;
+	}
+	return (m);
+}
