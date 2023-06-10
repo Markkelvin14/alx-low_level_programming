ba651c4c7b40ace2ccd16fd0c171cf2ff9fb1642
Markkelvin14 <mjkelvin38@gmail.com>
2023-06-10 12:14:12 +0000

    Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,7 +6,7 @@
 /**
  * hash_table_create - creates a hash table
  * @size: size of the array
- * return: a pointer to the new hash table
+ * Return: a pointer to the new hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
